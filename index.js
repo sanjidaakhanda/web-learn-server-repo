d5df269e41c599246e9217caa9a31f0ae42337f0
Your Name <sanjida.ier@gmail.com>
2022-10-25 23:04:33 -0400

    courses id added commit

--- index.js
+++ index.js
@@ -6,13 +6,25 @@ app.use(cors());
 const Port = process.env.Port || 5000;
 
 const categories = require("./Data/categories.json");
-
+const courses = require("./Data/courses.json");
 app.get("/", (req, res) => {
   res.send("now web learn is running");
 });
 
 app.get("/courses-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const singleCourse = courses.find((c) => c.id === id);
+  res.send(singleCourse);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const coursesCategory = courses.filter((p) => p.course_id === id);
+  res.send(coursesCategory);
 });
 
 app.listen(Port, () => {
